d8e98edb3e7c16abc778e11a2de1e4729d2733bd
Pranav.Sharma <contactpranav25@yahoo.co.uk>
2018-08-16 18:16:25 +0530

    initial Commit

---
+++ additionApp/src/main/java/cloudnative/microservices/additionApp/AdditionAppApplication.java
@@ -0,0 +1,12 @@
+package cloudnative.microservices.additionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionAppApplication.class, args);
+	}
+}
---
+++ additionApp/src/test/java/cloudnative/microservices/additionApp/AdditionAppApplicationTests.java
@@ -0,0 +1,16 @@
+package cloudnative.microservices.additionApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdditionAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
